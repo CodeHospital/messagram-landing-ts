aa5c54ba5cf851dc76173cfcd8bc65a2b3004ef5
Maysam Torabi <kherad@gmail.com>
2024-12-23 06:01:43 +0000

    adding apk to the site

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,15 +8,14 @@ export default function NotFound() {
     <Layout>
       <Container className="relative isolate flex h-full flex-col items-center justify-center py-20 text-center sm:py-32">
         <CirclesBackground className="absolute left-1/2 top-1/2 -z-10 mt-44 w-[68.125rem] -translate-x-1/2 -translate-y-1/2 stroke-gray-300/30 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)]" />
-        <p className="text-sm font-semibold text-gray-900">404</p>
         <h1 className="mt-2 text-3xl font-medium tracking-tight text-gray-900">
-          Page not found
+          Welcome to Messagram
         </h1>
-        <p className="mt-2 text-lg text-gray-600">
-          Sorry, we couldn’t find the page you’re looking for.
+        <p className="mt-4 text-lg text-gray-500">
+          Download the app to accept the invitation
         </p>
-        <Button href="/" variant="outline" className="mt-8">
-          Go back home
+        <Button href="/messagram.apk" variant="outline" className="mt-8">
+          DOWNLOAD
         </Button>
       </Container>
     </Layout>
