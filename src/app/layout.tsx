b47639e4066dde38ad2c9e861fc5acb948d697f7
Maysam Torabi <kherad@gmail.com>
2025-02-27 06:40:55 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-import { type Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import clsx from 'clsx'
+import { type Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import clsx from 'clsx';
+import Head from 'next/head'; // Import for dynamic head content
 
-import '@/styles/tailwind.css'
+import '@/styles/tailwind.css';
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-inter',
-})
+});
 
 export const metadata: Metadata = {
   title: {
@@ -17,21 +18,24 @@ export const metadata: Metadata = {
   },
   description:
     'By leveraging insights from our network of industry insiders, you’ll know exactly when to buy to maximize profit, and exactly when to sell to avoid painful losses.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={clsx('bg-gray-50 antialiased', inter.variable)}>
-      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2499635055097290"
-     crossorigin="anonymous"></script>
-        <meta name="google-adsense-account" content="ca-pub-2499635055097290">
-      </head>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2499635055097290"
+          crossOrigin="anonymous"
+        />
+        <meta name="google-adsense-account" content="ca-pub-2499635055097290" />
+      </Head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
