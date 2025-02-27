3aa307c83ce3b99a95ee144d864ac56a64dc5f95
Maysam Torabi <kherad@gmail.com>
2025-02-27 06:28:09 +0100

    Update layout.tsx
    adsense verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={clsx('bg-gray-50 antialiased', inter.variable)}>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2499635055097290"
+     crossorigin="anonymous"></script>
+        <meta name="google-adsense-account" content="ca-pub-2499635055097290">
+      </head>
       <body>{children}</body>
     </html>
   )
